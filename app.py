daf303a455bc2fb3fdce053c2a1b38adfb786333
Lucas Jordão <101470104+lucasjordaoreal@users.noreply.github.com>
2024-10-29 01:18:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from epic_games_spider import EpicGamesSpider
+import os
 
 app = Flask(__name__)
 
@@ -9,5 +10,5 @@ def index():
     games = spider.parse()  # Agora obtém a lista de jogos corretamente
     return render_template('index.html', games=games)  # Passa os jogos para o template
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
